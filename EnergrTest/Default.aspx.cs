7b384047aeefcd35d7c77e38bcaf3200944c9b6d
jyotiBankar01 <99374170+jyotiBankar01@users.noreply.github.com>
2022-02-10 21:36:15 +0530

    Update Default.aspx.cs

--- EnergrTest/Default.aspx.cs
+++ EnergrTest/Default.aspx.cs
@@ -30,70 +30,27 @@ namespace EnergrTest
             try
             {
                 DataTable datatbl = new DataTable();
-                if (ViewState["Row"] != null)
+				if (ViewState["Row"] != null)
                 {
-                    datatbl = (DataTable)ViewState["Row"];
-                    DataRow dr = null;
-                    dr = datatbl.NewRow();
-                    if (datatbl.Rows.Count > 0)
-                    {
-                        string dateinput = txtDate.Text;
-                        DateTime datetime = DateTime.Parse(dateinput);
-                        DayOfWeek date = datetime.DayOfWeek;
-
-                        dr["Date"] = datetime; //DateTime.Parse(txtDate.Text);
-                        dr["Energy Type"] = DropDownList1.SelectedValue;
-
-                        if (date == DayOfWeek.Saturday || date == DayOfWeek.Sunday)
-                        {
-                            double prise = Double.Parse(txtPrise.Text);
-                            double discount = prise - (prise * 10 / 100);
-                            dr["Prise"] = discount;
-                        }
-                        else
-                        {
-                            dr["Prise"] = Double.Parse(txtPrise.Text);
-                        }
-                        
-                        datatbl.Rows.Add(dr);
-                        ViewState["Row"] = datatbl;
-                        GridView1.DataSource = ViewState["Row"];
-                        GridView1.DataBind();
-
-                    }
-                }
-                else
-                {
-                    datatbl.Columns.Add("Date", typeof(DateTime));
+				   datatbl = (DataTable)ViewState["Row"];
+				}else{
+				    datatbl.Columns.Add("Date", typeof(DateTime));
                     datatbl.Columns.Add(new DataColumn("Energy Type", typeof(string))); 
                     datatbl.Columns.Add("Prise", typeof(double));
-
-                    DataRow dr1 = datatbl.NewRow();
-                    dr1 = datatbl.NewRow();
-
-                    string dateinput = txtDate.Text;
-                    DateTime dt = DateTime.Parse(dateinput);
-                    DayOfWeek date = dt.DayOfWeek;
-
-                    dr1["Date"] = dt;
-                    dr1["Energy Type"] = DropDownList1.SelectedValue;
-
-                    if (date == DayOfWeek.Saturday || date == DayOfWeek.Sunday)
-                    {
-                        double prise = Double.Parse(txtPrise.Text);
-                        double discount = prise - (prise * 10 / 100);
-                        dr1["Prise"] = discount;
-                    }
-                    else
-                    {
-                        dr1["Prise"] = Double.Parse(txtPrise.Text);
-                    }
-
-                    datatbl.Rows.Add(dr1);
-                    ViewState["Row"] = datatbl;
-                    GridView1.DataSource = ViewState["Row"];
-                    GridView1.DataBind();
-                }
+				}
+				        string dateinput = txtDate.Text;
+                        DateTime datetime = DateTime.Parse(dateinput);
+                        DayOfWeek date = datetime.DayOfWeek;
+						double prise = Double.Parse(txtPrise.Text);
+						
+						DataRow dr = datatbl.NewRow();
+                        dr["Date"] = datetime; //DateTime.Parse(txtDate.Text);
+                        dr["Energy Type"] = DropDownList1.SelectedValue;
+						dr["Prise"] = (date == DayOfWeek.Saturday || date == DayOfWeek.Sunday) ? (prise - (prise * 10 / 100)) : prise;
+                        datatbl.Rows.Add(dr);
+                        ViewState["Row"] = datatbl;
+                        GridView1.DataSource = datatbl;
+                        GridView1.DataBind();
 
             }
 
@@ -103,6 +60,7 @@ namespace EnergrTest
             }
 
         }
+
 
        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
         {
